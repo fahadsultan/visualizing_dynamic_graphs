0b02471122900da334e1f2309adefe8b58a29627
Fahad Sultan <syedfahadsultan@gmail.com>
2023-08-19 11:12:53 -0400

    Before Fall 202

--- src/FDEB/script3.js
+++ src/FDEB/script3.js
@@ -22,15 +22,17 @@ const min_segments_range  = 3;          // 20
 const max_segments_range  = 10;         // 30
 
 // settle at a layout faster
-const alpha_decay         = 0.1;
+const alpha_decay         = 0.5;
 
 // nearby nodes attract each other
 const force_charge_many   = 10;         // 10
 const force_distance_max  = 0;         // scales.airports.range()[1] * 2
 // edges want to be as short as possible
 // prevents too much stretching
-const force_link_strength = 0.1;         // 0.7, 0.001
-const force_link_distance = 0;         // 100
+const force_link_strength = 1;         // 0.7, 0.001
+const force_link_distance = 1;         // 100
+
+const max_time = 12; // 13;
 
 const scales = {
   // used to scale airport bubbles
@@ -45,14 +47,16 @@ const scales = {
 };
 
 const stroke_width   = function(d, i){ return 5;    };  //flights[i].count**(-0.5/10**10) ;
-const stroke_opacity = function(d, i){ return 0.2; };
-const node_radius    = function(d, i){ return scales.airports(d.outgoing/100000);}
+const stroke_opacity = function(d, i){ return 0.3; };
+const node_radius    = function(d, i){ return scales.airports(d.outgoing/500);}
 
 // have these already created for easier drawing
 const g = {
   flights    : svg.select("g#flights"),
   airports_c : svg.select("g#airports_c"),
-  airports_f : svg.select("g#airports_f")
+  airports_f : svg.select("g#airports_f"),
+  airportText: svg.select("g#airportText"),
+  months:      svg.select("g#months")
 };
 
 // load the airport and flight data together
@@ -91,7 +95,7 @@ function typeFlight(flight) {
 function typeWalk(walk){
     walk.length = 0
     let start = false;
-    for(var i = 0; i < 3; i++){
+    for(var i = 0; i < max_time; i++){
         if(walk[i] != ""){
           if (start == false){
             start = true;
@@ -129,7 +133,54 @@ function processData(values) {
 
   drawAirports(airports_c);
   drawWalks(airports_f, airports_c, flights, walks, iata_f, iata_c);
+  drawMonths();
+  drawNames(airports_c);
 }   
+
+function drawMonths(){
+    var svg = d3.select('#months')
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("transform", "translate(100,100)");
+
+    var x = d3.scalePoint()
+      .domain(["JAN", "FEB", "MAR", "APRL", "MAY", 'JUN', 'JUL', 'AUG', 'SEPT', 'OCT','NOV', 'DEC'])         // This is what is written on the Axis: from 0 to 100
+      .range([85, 1200]);    
+    
+    svg
+      .append("g")
+      .style('font', '20px sans-serif')
+      //.style('font', 'bold')
+      .attr("transform", "translate(58,620)")
+      // .attr("y", 1000)
+      .style("stroke-width", 1)
+      .call(d3.axisBottom(x));
+}
+
+function drawNames(airports){
+  g.airportText.selectAll("text")
+    .data(airports, d => d.iata)
+    .enter()
+    .append('text')
+    .attr('class','airportName')
+    .attr("x", d => d.x - 80) // calculated on load
+    .attr("y", d => d.y)
+    .text(d => d.iata)
+
+  let airList = document.getElementsByClassName("airportName")
+
+  for(let i = 0; i < airList.length; i ++){
+    if((airList[i].innerHTML)[4]==1 && (airList[i].innerHTML)[5] == null){
+      airList[i].innerHTML.slice(0,3)
+    }
+    else{
+      airList[i].style.display = 'none'
+    }
+  }
+  g.airportText.selectAll("text")
+    .text(d => d.iata.slice(0,3))
+}
 
 function drawAirports(airports) {
 
@@ -192,6 +243,9 @@ function drawWalks(airports_f, airports_c, flights, walks, iata_f, iata_c) {
 
     console.log(bundle);
 
+    var defs = svg.append("defs");
+
+
     // https://github.com/d3/d3-shape#curveBundle
     let line = d3.line()
         .curve(d3.curveBundle)
@@ -204,12 +258,34 @@ function drawWalks(airports_f, airports_c, flights, walks, iata_f, iata_c) {
         .append("path")
         .attr("d", line)
         .attr("class", "flight")
-        .style("stroke", function(d) { 
-            if (d[0].cluster == d[d.length-1].cluster) {
-                return d[0].color; 
-            } else { 
-                return d[0].color; 
-            }
+        .style("stroke", function(d,i) {
+
+          var gradient = defs.append("linearGradient")
+            .attr("id", "svgGradient")
+            .attr("x1", "0%").attr("x2", "100%")
+            .attr("y1", "0%").attr("y2", "100%");
+      
+          gradient.append("stop")
+            .attr("class", "start")
+            .attr("offset", "0%")
+            .attr("stop-color", d[0].color)
+            .attr("stop-opacity", 1);
+      
+          gradient.append("stop")
+          .attr("class", "end")
+          .attr("offset", "100%")
+          .attr("stop-color", d[d.length-1].color)
+          .attr("stop-opacity", 1);
+           
+          // return "url(#svgGradient)"
+          // var myColor = d3.scaleSequential().domain([1,1000]).interpolator(d3.interpolateViridis);
+            // if (d[0].cluster == d[d.length-1].cluster) {
+            //     return myColor[d[0].color]; 
+            // } else { 
+            //     return "orange"; 
+            // }
+            // console.log(i);
+            return d[0].color;
         })
         .style("stroke-width", stroke_width)
         .style("stroke-opacity", stroke_opacity)
@@ -228,16 +304,21 @@ function drawWalks(airports_f, airports_c, flights, walks, iata_f, iata_c) {
         // settle at a layout faster
         .alphaDecay(alpha_decay)
         // nearby nodes attract each other
-        .force("charge", d3.forceManyBody()
-            .strength(force_charge_many)
-            .distanceMax(force_distance_max)
-        )
+        // .force("charge", d3.forceManyBody()
+        //     .strength(force_charge_many)
+        //     .distanceMax(force_distance_max)
+        // )
         // edges want to be as short as possible
         // prevents too much stretching
         .force("link", d3.forceLink()
             .strength(force_link_strength)
             .distance(force_link_distance)
         )
+        .force("x", d3.forceX().strength(function(d, i){
+          console.log((d.y));
+          return 0;
+        }))
+        // .force("y", d3.forceY(0).strength(0.5))
         // .force("center", 0)
         .on("tick", function(d) { links.attr("d", line); })
         .on("end", function(d)  { console.log("layout complete"); });
